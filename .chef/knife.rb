bb180b1050f6a530219f41d6c48195b2acbc590f
root <root@ip-172-31-27-77.us-west-2.compute.internal>
2018-03-07 13:52:38 +0000

    backup

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,7 +5,7 @@ node_name                "anish95"
 client_key               "#{current_dir}/anish95.pem"
 chef_server_url          "https://api.chef.io/organizations/streetblues"
 cookbook_path            ["#{current_dir}/../cookbooks"]
-knife[:aws_access_key_id] = "AKIAIGVBZACHCPL6H2EA"
-knife[:aws_secret_access_key] = "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"
+knife[:aws_access_key_id] = "AKIAJ2AY7YWAC5BX55OA"
+knife[:aws_secret_access_key] = "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"
 knife[:editor] = "/usr/bin/vim"
 
